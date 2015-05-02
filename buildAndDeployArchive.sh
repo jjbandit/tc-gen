dac868d20150fe11acf82da8339ad688f1733158
jjbandit <revelstokesnowboard@gmail.com>
2015-05-02 14:24:41 -0700

    optimize deploy script

--- buildAndDeployArchive.sh
+++ buildAndDeployArchive.sh
@@ -1,6 +1,17 @@
 #! /bin/sh
 
-zip tc-gen-archive.zip exampleTemplate.xlsx genericTemplate.xlsx target/tcGen-0.2.1-jar-with-dependencies.jar LICENSE.txt README.txt
-cp tc-gen-archive.zip /home/scallywag/www/jHugs/public/tc-gen-archive.zip;
+# Copy and rename the packed jar to the top level
+cp /home/scallywag/workspace/tc-gen/target/tcGen-*-jar-with-dependencies.jar ./timecardGenerator.jar
+
+# Create the archive
+zip tc-gen-archive.zip ./exampleTemplate.xlsx ./genericTemplate.xlsx ./timecardGenerator.jar ./LICENSE.txt ./README.txt
+
+# Remove the renamed jar
+rm timecardGenerator.jar
+
+# Move the new archive to the deploy location
+mv tc-gen-archive.zip /home/scallywag/www/jHugs/public/tc-gen-archive.zip;
+
+# And deploy
 cd /home/scallywag/www/jHugs;
 mup deploy;
